c9cfac38e5fad1d8de77e32f66abdde729876809
Pete Vilter <pete.vilter@gmail.com>
2022-02-16 17:07:46 -0500

    switch slow and fast

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,22 +4,9 @@ using Test
 using Profile
 using PProf
 
-# x = [[1,2], [3,4]]
 x = [collect(1:100) for i in 1:100]
 
 @testset "SimpleCSV" begin
-
-    @testset "fast" begin
-        # once to warm up...
-        path, io = mktemp()
-        SimpleCSV.Fast.serialize(io, x)
-
-        # ...and once under profiler
-        Profile.Allocs.clear()
-        path, io = mktemp()
-        @time Profile.Allocs.@profile sample_rate=1 SimpleCSV.Fast.serialize(io, x)
-        PProf.Allocs.pprof(out="fast", web=false)
-    end
 
     @testset "slow" begin
         # once to warm up...
@@ -31,6 +18,18 @@ x = [collect(1:100) for i in 1:100]
         Profile.Allocs.clear()
         @time Profile.Allocs.@profile sample_rate=1 SimpleCSV.Slow.serialize(io, x)
         PProf.Allocs.pprof(out="slow", web=false)
+    end
+
+    @testset "fast" begin
+        # once to warm up...
+        path, io = mktemp()
+        SimpleCSV.Fast.serialize(io, x)
+
+        # ...and once under profiler
+        Profile.Allocs.clear()
+        path, io = mktemp()
+        @time Profile.Allocs.@profile sample_rate=1 SimpleCSV.Fast.serialize(io, x)
+        PProf.Allocs.pprof(out="fast", web=false)
     end
 
     # to diff the two profiles:
