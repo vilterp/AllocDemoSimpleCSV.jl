06d607de15c5a4fcd2aa1ae4b0d1c22eb95538e1
Pete Vilter <pete.vilter@gmail.com>
2022-02-16 16:38:09 -0500

    add tests under profiler for both

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,30 @@
 using SimpleCSV
 
+using Test
+using Profile
+using PProf
+
 x = [[1,2], [3,4]]
 
-@time SimpleCSV.Fast.serialize(stdout, x)
+@testset "SimpleCSV" begin
 
-@time SimpleCSV.Slow.serialize(stdout, x)
+  @testset "fast" begin
+    # once to warm up...
+    SimpleCSV.Fast.serialize(stdout, x)
+
+    # ...and once under profiler
+    Profile.Allocs.clear()
+    Profile.Allocs.@profile sample_rate=1 SimpleCSV.Fast.serialize(stdout, x)
+    PProf.Allocs.pprof(out="fast", web=false)
+  end
+
+  @testset "slow" begin
+    # once to warm up...
+    SimpleCSV.Slow.serialize(stdout, x)
+
+    # ...and once under profiler
+    Profile.Allocs.clear()
+    Profile.Allocs.@profile SimpleCSV.Slow.serialize(stdout, x)
+    PProf.Allocs.pprof(out="slow", web=false)
+  end
+end
