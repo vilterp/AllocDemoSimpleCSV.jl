73cb22ad11278d9b26724249e6b8c54bec7bd830
Pete Vilter <pete.vilter@gmail.com>
2022-07-06 15:23:17 -0400

    fix spacing

--- src/slow.jl
+++ src/slow.jl
@@ -1,22 +1,22 @@
 module Slow
 
 function serialize(io::IO, rows::Vector{Vector{T}}) where T
-  print(io, serialize(rows))
+    print(io, serialize(rows))
 end
 
 function serialize(rows::Vector{Vector{T}}) where T
-  output = ""
-  for row in rows
-      for col_idx in 1:length(row)
-          val = row[col_idx]
-          output = output * string(val)
-          if col_idx < length(row)
+    output = ""
+    for row in rows
+        for col_idx in 1:length(row)
+            val = row[col_idx]
+            output = output * string(val)
+            if col_idx < length(row)
               output = output * ","
-          end
-      end
-      output = output * "\n"
-  end
-  return output
+            end
+        end
+        output = output * "\n"
+    end
+    return output
 end
 
 end # module
